2392e602d5f25b73cdc220d8e1f2b198f4581321
silviooosilva <gasparsilvio7@gmail.com>
2022-08-15 21:04:57 +0100

    Primeiro exemplo com rotas yaml

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -2,12 +2,14 @@
 
 namespace App\Controller;
 
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
 
+    /*
     public function number(): Response
     {
         $number = random_int(0, 100);
@@ -15,5 +17,30 @@ class LuckyController
         return new Response(
             '<html><body>Lucky Number: ' . $number . '</body></html>'
         );
+    }
+    */
+
+    /**
+     * ROTA COM O ANNOTATIONS
+     */
+
+    #[Route('/lucky/number')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+        /*
+        return new Response(
+            '<html><body>Lucky Number: ' . $number . '</body></html>'
+        );
+        
+        Forma de renderizar o conteúdo com o sistema de templates do symfony TWIG
+
+        ABAIXO
+
+        */
+
+        return $this->render('number.html.twig', [
+            'number' => $number,
+        ]);
     }
 }
